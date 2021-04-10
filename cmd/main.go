154ddcb5692fd5b8b1941c612edaa1fab68e478a
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-04-09 19:25:21 -0500

    suma test

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/whiteagleinc-meli/curso-testing-go/clase1"
+)
+
 func main() {
 	suma := clase1.Add(2, 3)
+
+	if suma != 5 {
+		log.Printf("Error: Se esperaba %d, se obtuvo %d", 5, add)
+		os.Exit(1)
+	}
+
+	log.Println("Todo salio bien")
 }
